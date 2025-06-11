28e274614487cf630050447aa1d31e268176bbdf
dabadav <dante.avino@alum.esci.upf.edu>
2025-06-11 17:47:39 +0200

    Added reload cli entrypoint suppor for development, also handling exceptions in /recommend and new endpoint /compute_metrics

--- src/ai_cdss_api/cli.py
+++ src/ai_cdss_api/cli.py
@@ -1,15 +1,50 @@
 # ai_cdss_api/cli.py
 import typer
 import uvicorn
+import logging
 
-cli = typer.Typer()
+logging.basicConfig(
+    level=logging.DEBUG, # Default to INFO if not specified via CLI
+    format='%(levelname)s:%(name)s:%(message)s'
+)
+logger = logging.getLogger(__name__)
+
+cli = typer.Typer(
+    help="CLI for the AI-CDSS FastAPI server."
+)
 
 @cli.command()
 def run(
     host: str = typer.Option("127.0.0.1", help="Host to bind the server to."),
     port: int = typer.Option(8000, help="Port to run the server on."),
-    reload: bool = typer.Option(True, help="Enable auto-reload.")
+    reload: bool = typer.Option(True, help="Enable auto-reload."),
+    reload_dirs: list[str] = typer.Option(
+        None, # Default to None, so typer knows it can collect multiple values
+        "--reload-dir",
+        "-rd",
+        help="Additional directories to watch for reload. Can be specified multiple times.",
+        rich_help_panel="Reload Options"
+    ),
+    log_level: str = typer.Option(
+        "info",
+        "--log-level",
+        "-l",
+        help="Set the logging level (debug, info, warning, error, critical).",
+        rich_help_panel="Logging Options"
+    )
 ):
     """Start the FastAPI server."""
-    uvicorn.run("ai_cdss_api.main:app", host=host, port=port, reload=reload)
+    # Convert string level (e.g., "debug") to Python's numeric logging level (e.g., logging.DEBUG)
+    # numeric_level = getattr(logging, log_level.upper(), None)
+
+    # # Validate if the provided log level string is valid
+    # if not isinstance(numeric_level, int):
+    #     logger.error(f"Invalid log level specified: '{log_level}'. Defaulting to INFO.")
+    #     numeric_level = logging.INFO
+
+    # # Set the level for the root logger. This affects all loggers throughout your application.
+    # logging.getLogger().setLevel(numeric_level)
+    # logger.info(f"Application logging level set to: {logging.getLevelName(numeric_level)}")
+
+    uvicorn.run("ai_cdss_api.main:app", host=host, port=port, reload=reload, reload_dirs=reload_dirs if reload_dirs else None)
 
--- src/ai_cdss_api/main.py
+++ src/ai_cdss_api/main.py
@@ -1,13 +1,14 @@
-from fastapi import FastAPI, Depends, HTTPException, Request
+from fastapi import FastAPI, Depends, HTTPException, status, Request
 from contextlib import asynccontextmanager
 
 from ai_cdss_api.config import Settings
 from ai_cdss_api.dependencies import get_settings
-from ai_cdss_api.schemas import RecommendationRequest, RGSMode
+from ai_cdss_api.schemas import RecommendationRequest
 
 from ai_cdss.cdss import CDSS
-from ai_cdss.data_loader import DataLoader
-from ai_cdss.data_processor import DataProcessor
+from ai_cdss.data_loader import DataLoader, DataLoaderLocal
+from ai_cdss.data_processor import DataProcessor, ClinicalSubscales, ProtocolToClinicalMapper, compute_ppf
+from ai_cdss.constants import PPF_PARQUET_FILEPATH
 
 from rgs_interface.data.schemas import PrescriptionStagingRow, RecsysMetricsRow
 
@@ -57,7 +58,7 @@ app = FastAPI(
     tags=["Recommendations"]
     )
 def recommend(
-    request: Request, 
+    request: Request,
     payload: RecommendationRequest,
     settings: Settings = Depends(get_settings),
 ):
@@ -72,64 +73,175 @@ def recommend(
     protocols_per_day = payload.protocols_per_day or settings.PROTOCOLS_PER_DAY
 
     # study_id -> patient_list
-    patient_data = loader.interface.fetch_patients_by_study(study_ids = study_id)
-    if patient_data.empty:
+    try:
+        patient_data = loader.interface.fetch_patients_by_study(study_ids = study_id)
+        if patient_data.empty:
+            raise HTTPException(
+                status_code=404,
+                detail=f"No patients found for study ID: {study_id}"
+            )
+        else:
+            patient_list = patient_data["PATIENT_ID"].tolist()
+        logger.info(f"Fetched {len(patient_list)} patients for study ID {study_id}")
+
+        # ** LOADING DATA ** #
+        session = loader.load_session_data(patient_list=patient_list)
+        timeseries = loader.load_timeseries_data(patient_list=patient_list)
+        ppf = loader.load_ppf_data(patient_list=patient_list)
+        protocol_similarity = loader.load_protocol_similarity()
+        
+        # ** PROCESSING DATA ** #
+        # SessionSchema, TimeseriesSchema, PPFSchema -> ScoringSchema
+        scores = processor.process_data(session_data=session, timeseries_data=timeseries, ppf_data=ppf, init_data=None)
+        #  TODO: Add whole scoring to db
+
+        # ** BUSINESS LOGIC ** #
+        cdss = CDSS(scoring=scores, n=n, days=days, protocols_per_day=protocols_per_day)
+        unique_id = uuid.uuid4()
+        datetime_now = datetime.datetime.now()
+
+        for patient in patient_list:
+
+            recommendations = cdss.recommend(patient, protocol_similarity)
+
+            prescription_df = (
+                recommendations
+                .explode("DAYS")
+                .rename(columns={"DAYS": "WEEKDAY"})
+            )
+            metrics_df = pd.melt(
+                recommendations,
+                id_vars=["PATIENT_ID", "PROTOCOL_ID"],
+                value_vars=["DELTA_DM", "ADHERENCE_RECENT", "PPF"],
+                var_name="KEY",
+                value_name="VALUE"
+            )
+
+            # ** DB WRITING ** #
+            for _, row in prescription_df.iterrows():
+                loader.interface.add_prescription_staging_entry(
+                    PrescriptionStagingRow.from_row(
+                        row,
+                        recommendation_id=unique_id,
+                        start=datetime_now,
+                    )
+                )
+
+            for _, row in metrics_df.iterrows():
+                loader.interface.add_recsys_metric_entry(
+                    RecsysMetricsRow.from_row(
+                        row, 
+                        recommendation_id=unique_id,
+                        metric_date=datetime_now,
+                    )
+                )
+
+            return {"message": "Recommendations generated and processed successfully."}
+
+    except HTTPException:
+        # Re-raise HTTPExceptions that were intentionally raised by previous try-except blocks
+        raise
+    except Exception as e:
+        # This catches any unhandled exceptions that slip through the more specific blocks
+        logger.critical(f"An unexpected and unhandled error in the recommend endpoint for study ID {study_id}: {e}", exc_info=True)
         raise HTTPException(
-            status_code=404,
-            detail=f"No patients found for study ID: {study_id}"
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Unexpected internal server error during recommendation generation. {e}"
         )
-    else:
-        patient_list = patient_data["PATIENT_ID"].tolist()
-    logger.info(f"Fetched {len(patient_list)} patients for study ID {study_id}")
 
-    # ** LOADING DATA ** #
-    session = loader.load_session_data(patient_list=patient_list)
-    timeseries = loader.load_timeseries_data(patient_list=patient_list)
-    ppf = loader.load_ppf_data(patient_list=patient_list)
-    protocol_similarity = loader.load_protocol_similarity()
+@app.post(
+    "/compute_metrics/{patient_id}", 
+    summary="Compute Patient-Protocol Fit",
+    tags=["Recommendations"]
+    )
+async def compute_metrics(
+    patient_id: int,
+    request: Request,
+):
+    """
+    Computes Patient-Protocol Fit (PPF) and Protocol Similarity based on loaded data.
+    Returns the computed PPF with contributions and the protocol similarity matrix.
+    """
+
+    # Load Patient Subscale Scores, and Protocol Attributes
+    try:
+        loader: DataLoaderLocal = request.app.state.loader
+
+        patient = loader.load_patient_subscales([patient_id])
+        if patient.empty:
+            logger.critical(f"Patient data not found for ID: {patient_id}")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Patient data not found for ID: {patient_id}"
+            )
+
+        protocol = loader.load_protocol_attributes()
+        if protocol.empty:
+            logger.critical("Protocol data could not be loaded.")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Protocol data could not be loaded."
+            )
+
+        patient_deficiency = ClinicalSubscales().compute_deficit_matrix(patient)
+        protocol_mapped = ProtocolToClinicalMapper().map_protocol_features(protocol)
+
+        ppf, contrib = compute_ppf(patient_deficiency, protocol_mapped)
+        ppf_contrib = pd.merge(ppf, contrib, on=["PATIENT_ID", "PROTOCOL_ID"], how="left")
+        ppf_contrib.attrs = {"SUBSCALES": list(protocol_mapped.columns)}
+
+        # Append to PPF parquet
+        if not ppf_contrib.empty:
+            try:
+                # Check if the file already exists to decide initial write mode
+                if not PPF_PARQUET_FILEPATH.exists():
+                    # If file doesn't exist, create it (write mode)
+                    logger.debug(f"Creating new file {PPF_PARQUET_FILEPATH}")
+                    ppf_contrib.to_parquet(PPF_PARQUET_FILEPATH, index=False)
+                
+                # Perform upsert operation, appending new record or updating on previous patient, protocol.
+                else:
+                    # Read existing data
+                    existing_ppf = pd.read_parquet(PPF_PARQUET_FILEPATH)
+
+                    # Identify rows in existing_ppf that are NOT being updated by ppf_contrib
+                    new_or_updated_keys = ppf_contrib[['PATIENT_ID', 'PROTOCOL_ID']]
+                    # Anti-join to keep only old rows that DON'T have a match in new_or_updated_keys
+                    merged = existing_ppf.merge(new_or_updated_keys, on=['PATIENT_ID', 'PROTOCOL_ID'], how='left', indicator=True)
+                    filtered_existing_ppf = existing_ppf[merged['_merge'] == 'left_only']
+                    updated_ppf = pd.concat([filtered_existing_ppf, ppf_contrib], ignore_index=True)
+
+                    # Propagate metadata
+                    updated_ppf.attrs = {"SUBSCALES": list(protocol_mapped.columns)}
+
+                    # Overwrite the file with combined data
+                    updated_ppf.to_parquet(PPF_PARQUET_FILEPATH)
+
+                saved_path = str(PPF_PARQUET_FILEPATH.absolute())
+                logger.debug(f"Appended results for patient {patient_id} to single Parquet file: {saved_path}")
+
+            except Exception as parquet_err:
+                logger.error(f"Error appending to single Parquet file for patient {patient_id}: {parquet_err}")
+                raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                    detail=f"Failed to save results to Parquet: {parquet_err}")
     
-    # ** PROCESSING DATA ** #
-    scores = processor.process_data(session_data=session, timeseries_data=timeseries, ppf_data=ppf, init_data=None) # SessionSchema, TimeseriesSchema, PPFSchema -> ScoringSchema
-    #  TODO: Add whole scoring to db
+        else:
+            print(f"No PPF data to save for patient {patient_id}. Skipping Parquet write.")
 
-    # ** BUSINESS LOGIC ** #
-    cdss = CDSS(scoring=scores, n=n, days=days, protocols_per_day=protocols_per_day)
-    unique_id = uuid.uuid4()
-
-    for patient in session.PATIENT_ID.unique():
-
-        recommendations = cdss.recommend(patient, protocol_similarity)
-        prescription_df = (
-            recommendations
-            .explode("DAYS")
-            .rename(columns={"DAYS": "WEEKDAY"})
+        return {
+            "message": f"Computation successful for patient {patient_id}, updated file {saved_path}",
+            "patient_id": patient_id,
+            "subscales_used": list(protocol_mapped.columns) # Include metadata here
+        }
+    
+    except HTTPException as e:
+        raise e
+    
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Unexpected error: {str(e)}"
         )
-        metrics_df = pd.melt(
-            recommendations,
-            id_vars=["PATIENT_ID", "PROTOCOL_ID"],
-            value_vars=["DELTA_DM", "ADHERENCE_RECENT", "PPF"],
-            var_name="KEY",
-            value_name="VALUE"
-        )
-
-        # ** DB WRITING ** #
-        for _, row in prescription_df.iterrows():
-            loader.interface.add_prescription_staging_entry(
-                PrescriptionStagingRow.from_row(
-                    row,
-                    recommendation_id=unique_id,
-                    start=datetime.datetime.now(),
-                )
-            )
-
-        for _, row in metrics_df.iterrows():
-            loader.interface.add_recsys_metrics_entry(
-                RecsysMetricsRow.from_row(
-                    row, 
-                    recommendation_id=unique_id,
-                    metric_date=datetime.datetime.now(),
-                )
-            )
 
 ###
 ### Chron -> patient info / study -> fetch_data -> process data -> write data
